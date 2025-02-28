fb30dc552319274f5949d1d369a06b566b7e41a8
Michael <mikerhogg@gmail.com>
2025-02-27 21:24:59 -0700

    Skeleton of endpoints implemented

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -10,9 +10,16 @@ public class Server {
         Spark.staticFiles.location("web");
 
         // Register your endpoints and handle exceptions here.
+        Spark.delete("/db",(req,res)->"Delete all stuff");
+        Spark.post("/user",(req,res)->"Add user");
+        Spark.post("/session",(req,res)->"Login");
+        Spark.delete("/session",(req,res)->"Logout");
+        Spark.get("/game",(req,res)->"List all games");
+        Spark.post("/game",(req,res)->"Create game");
+        Spark.put("/game",(req,res)->"Join Game");
 
         //This line initializes the server and can be removed once you have a functioning endpoint 
-        Spark.init();
+       // Spark.init();
 
         Spark.awaitInitialization();
         return Spark.port();
